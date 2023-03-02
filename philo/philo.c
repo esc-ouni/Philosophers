005d7fac24778c4800d52d0f739e0ddb0ccdd2be
idouni <idouni@student.1337.ma>
2023-03-02 16:58:32 +0100

    philosohers

--- philo/philo.c
+++ philo/philo.c
@@ -12,9 +12,42 @@
 
 #include "philo.h"
 
+void	NULL1()
+{
+	printf("thread uno\n");
+	printf("bye uno\n\n");
+	pthread_exit(NULL);
+}
+
+void	NULL2()
+{
+	printf("thread dos\n");
+	printf("bye dos\n\n");
+	pthread_exit(NULL);
+}
+
+void	NULL3()
+{
+	printf("hread trs\n");
+	printf("bye trs\n\n");
+	pthread_exit(NULL);
+}
+
 int	main()
 {
-	tec:w!
-	
+	pthread_t	thread;
+	pthread_t	thread1;
+	pthread_t	thread2;
+
+	pthread_create(&thread, NULL, (void *)NULL1, NULL);
+	pthread_create(&thread1, NULL, (void *)NULL2, NULL);
+	pthread_create(&thread2, NULL, (void *)NULL3, NULL);
+
+	pthread_join(thread, NULL);
+	pthread_join(thread1, NULL);
+	pthread_join(thread2, NULL);
+
+	pthread_exit(NULL);
+
 	return (0);
 }
