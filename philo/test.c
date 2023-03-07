7a8385ec9ddf76f9db5b9e2b328ec265b50ec5c8
idouni <idouni@student.1337.ma>
2023-03-07 13:08:56 +0100

    philosohers

--- philo/test.c
+++ philo/test.c
@@ -20,31 +20,35 @@ int	main()
 	// int	i;
 
 	// i = 0;
+	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 
-	time_t	p;
-	time_t	p1;
-	struct timezone tzp;
-	struct timeval tp;
+	pthread_mutex_init(&mutex, NULL);
+	pthread_mutex_lock(&mutex);
+	pthread_mutex_unlock(&mutex);
+	// time_t	p;
+	// time_t	p1;
+	// struct timezone tzp;
+	// struct timeval tp;
 
 	// pthread_create(&thread, NULL, (void *)NULL1, NULL);
 	// // printf("%lu\n", thread);
 	// pthread_join(thread, NULL);
 	// printf("%lu\n", thread);
 	// sleep(2);
-	gettimeofday(&tp, &tzp);
-	p = tp.tv_sec;
-	printf("%lu\n", p);
-	sleep(2);
-	gettimeofday(&tp, &tzp);
-	p1 = tp.tv_sec - p;
-	printf("%lu\n", p1);
-	sleep(2);
-	gettimeofday(&tp, &tzp);
-	p1 = tp.tv_sec - p;
-	printf("%lu\n", p1);
-	sleep(2);
-	gettimeofday(&tp, &tzp);
-	p1 = tp.tv_sec - p;
-	printf("%lu\n", p1);
+	// gettimeofday(&tp, &tzp);
+	// p = tp.tv_sec;
+	// printf("%lu\n", p);
+	// sleep(2);
+	// gettimeofday(&tp, &tzp);
+	// p1 = tp.tv_sec - p;
+	// printf("%lu\n", p1);
+	// sleep(2);
+	// gettimeofday(&tp, &tzp);
+	// p1 = tp.tv_sec - p;
+	// printf("%lu\n", p1);
+	// sleep(2);
+	// gettimeofday(&tp, &tzp);
+	// p1 = tp.tv_sec - p;
+	printf("%lu\n", mutex);
 	return (0);
 }
