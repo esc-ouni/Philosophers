664c827bdaa0cdde9ced9d8edcf183d8e06e5f78
idouni <idouni@student.1337.ma>
2023-03-22 11:22:01 +0000

    philosohers

--- philo/tools_3.c
+++ philo/tools_3.c
@@ -12,6 +12,30 @@
 
 #include "philo.h"
 
+void	philosopher_state_2(void *arg)
+{
+	t_list			*node;
+
+	node = (t_list  *)arg;
+	pthread_mutex_lock(node->l_fork);
+	pthread_mutex_lock(node->r_fork);
+	node->lock_status = LOCKED;
+	node->next->lock_status = LOCKED;
+	printer(node, "has taken a fork");
+	printer(node, "has taken a fork");		
+	printer(node, "is eating");
+	node->eaten_meals++;
+	usleep(node->time_to_eat * 1000);
+	node->time_left += ft_time();
+	pthread_mutex_unlock(node->l_fork);
+	node->lock_status = UNLOCKED;
+	pthread_mutex_unlock(node->r_fork);
+	node->next->lock_status = UNLOCKED;
+	printer(node, "is sleeping");
+	usleep(node->time_to_sleep * 1000);
+	node->old_status = THINKING_STATE;
+}
+
 void	*philosopher_state(void *arg)
 {
 	t_list			*node;
@@ -32,25 +56,7 @@ void	*philosopher_state(void *arg)
 			node->old_status = ALREADY_THINKING;
 		}
 		if (node->lock_status == UNLOCKED && node->next->lock_status == UNLOCKED)
-		{
-			pthread_mutex_lock(node->l_fork);
-			pthread_mutex_lock(node->r_fork);
-			node->lock_status = LOCKED;
-			node->next->lock_status = LOCKED;
-			printer(node, "has taken a fork");
-			printer(node, "has taken a fork");		
-			printer(node, "is eating");
-			node->eaten_meals++;
-			usleep(node->time_to_eat * 1000);
-			node->time_left += ft_time();
-			pthread_mutex_unlock(node->l_fork);
-			node->lock_status = UNLOCKED;
-			pthread_mutex_unlock(node->r_fork);
-			node->next->lock_status = UNLOCKED;
-			printer(node, "is sleeping");
-			usleep(node->time_to_sleep * 1000);
-			node->old_status = THINKING_STATE;
-		}
+			philosopher_state_2(arg);
 	}
 	node->eat_state = EAT_ENOUGH;
 	return (NULL);
