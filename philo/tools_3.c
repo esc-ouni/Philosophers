a67809835ea6eea459e86e47861166303fcdabec
idouni <idouni@student.1337.ma>
2023-03-22 10:48:07 +0000

    philosohers

--- philo/tools_3.c
+++ philo/tools_3.c
@@ -12,6 +12,54 @@
 
 #include "philo.h"
 
+void    philosopher_state_2(void *arg)
+{
+	int				max;
+	int				id;
+	t_list			*node;
+	pthread_mutex_t	*l_fork;
+	pthread_mutex_t	*r_fork;
+	// t_list			*node;
+
+	max = 0;
+	node = (t_list  *)arg;
+	l_fork = &node->l_fork;
+	r_fork = &node->next->l_fork;
+	id = node->philosopher_id;
+	node->eaten_meals = 0;
+	node->time_left = node->time_to_think;
+
+	node = (t_list  *)arg;
+	if (node->lock_status == UNLOCKED && \
+	node->next->lock_status == UNLOCKED && node->eaten_meals <= max)
+	{
+		pthread_mutex_lock(l_fork);
+		node->lock_status = LOCKED;
+		if (node->next->lock_status == LOCKED)
+		{
+			pthread_mutex_unlock(l_fork);
+			// continue ;
+		}
+		pthread_mutex_lock(r_fork);
+		node->next->lock_status = LOCKED;
+		printer(node, "has taken a fork");
+		printer(node, "has taken a fork");		
+		printer(node, "is eating");
+		node->eaten_meals++;
+		if (node->eaten_meals > max)
+			max = node->eaten_meals;
+		usleep(node->time_to_eat * 1000);
+		node->time_left += ft_time();
+		pthread_mutex_unlock(l_fork);
+		node->lock_status = UNLOCKED;
+		pthread_mutex_unlock(r_fork);
+		node->next->lock_status = UNLOCKED;
+		printer(node, "is sleeping");
+		usleep(node->time_to_sleep * 1000);
+		node->old_status = THINKING_STATE;
+	}
+}
+
 void	*philosopher_state(void *arg)
 {
 	int				max;
@@ -22,8 +70,8 @@ void	*philosopher_state(void *arg)
 
 	// printf("\n%p\n%p\n%p\n%p\n\n", max, id, node, l_fork, r_fork);
 
-	max = 0;
 	ft_time();
+	max = 0;
 	node = (t_list  *)arg;
 	l_fork = &node->l_fork;
 	r_fork = &node->next->l_fork;
@@ -42,34 +90,34 @@ void	*philosopher_state(void *arg)
 			printer(node, "is thinking");
 			node->old_status = ALREADY_THINKING;
 		}
-		if (node->lock_status == UNLOCKED && \
-		node->next->lock_status == UNLOCKED && node->eaten_meals <= max)
-		{
-			pthread_mutex_lock(l_fork);
-			node->lock_status = LOCKED;
-			if (node->next->lock_status == LOCKED)
-			{
-				pthread_mutex_unlock(l_fork);
-				continue ;
-			}
-			pthread_mutex_lock(r_fork);
-			node->next->lock_status = LOCKED;
-			printer(node, "has taken a fork");
-			printer(node, "has taken a fork");		
-			printer(node, "is eating");
-			node->eaten_meals++;
-			if (node->eaten_meals > max)
-				max = node->eaten_meals;
-			usleep(node->time_to_eat * 1000);
-			node->time_left += ft_time();
-			pthread_mutex_unlock(l_fork);
-			node->lock_status = UNLOCKED;
-			pthread_mutex_unlock(r_fork);
-			node->next->lock_status = UNLOCKED;
-			printer(node, "is sleeping");
-			usleep(node->time_to_sleep * 1000);
-			node->old_status = THINKING_STATE;
-		}
+		philosopher_state_2(arg);
+		// if (node->lock_status == UNLOCKED && \
+		// node->next->lock_status == UNLOCKED && node->eaten_meals <= max)
+		// {
+		// 	pthread_mutex_lock(l_fork);
+		// 	node->lock_status = LOCKED;
+		// 	if (node->next->lock_status == LOCKED)
+		// 	{
+		// 		pthread_mutex_unlock(l_fork);
+		// 		continue ;
+		// 	}
+		// 	pthread_mutex_lock(r_fork);
+		// 	node->next->lock_status = LOCKED;
+		// 	printer(node, "has taken a fork");
+		// 	printer(node, "has taken a fork");		
+		// 	printer(node, "is eating");
+		// 	node->eaten_meals++;
+		// 	if (node->eaten_meals > max)
+		// 		max = node->eaten_meals;
+		// 	usleep(node->time_to_eat * 1000);
+		// 	node->time_left += ft_time();
+		// 	pthread_mutex_unlock(l_fork);
+		// 	node->lock_status = UNLOCKED;
+		// 	pthread_mutex_unlock(r_fork);
+		// 	node->next->lock_status = UNLOCKED;
+		// 	printer(node, "is sleeping");
+		// 	usleep(node->time_to_sleep * 1000);
+		// 	node->old_status = THINKING_STATE;
 	}
 	return (NULL);
 }
